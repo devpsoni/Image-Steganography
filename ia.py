799503af8152823aa9f93899658a6c2b5575ff79
Dev Soni <43056706+devpsoni@users.noreply.github.com>
2024-02-12 11:36:19 +0530

    Fixed bugs.

--- ia.py
+++ ia.py
@@ -1,64 +1,61 @@
+# Importing necessary libraries
 from PIL import Image
 import numpy as np
 
-def message_to_bin(message: str) -> str:
-    """
-    Convert a message to a binary string.
-    """
-    return ''.join(format(ord(char), '08b') for char in message)
+# Function to convert a message to binary
+def message_to_bin(message):
+    binary_message = ''.join(format(ord(char), '08b') for char in message)
+    return binary_message
 
-def bin_to_message(binary: str) -> str:
-    """
-    Convert a binary string to its message.
-    """
-    return ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
+# Function to convert binary to message
+def bin_to_message(binary):
+    message = ''.join(chr(int(binary[i:i+8], 2)) for i in range(0, len(binary), 8))
+    return message
 
-def encode_image(img_path: str, message: str, output_path: str) -> str:
-    """
-    Encode a message into the alpha channel of an image.
-    """
-    try:
-        img = Image.open(img_path)
-    except IOError:
-        return "Error opening image file."
-
-    binary_message = message_to_bin(message) + '1111111111111110'  # Delimiter
-
-    if img.mode != 'RGBA':
+# Function to encode a message into an image
+def encode_image(img_path, message, output_path):
+    img = Image.open(img_path)
+    binary_message = message_to_bin(message) + '1111111111111110' # Adding a delimiter to signify end of the message
+    if img.mode in ('RGBA'):
         img = img.convert('RGBA')
+        datas = img.getdata()
 
-    datas = np.array(img)
-    flat_data = datas.flatten()
-    message_len = len(binary_message)
+        newData = []
+        digit = 0
+        for item in datas:
+            if (digit < len(binary_message)):
+                newpix = item[:3] + (item[3] & ~1 | int(binary_message[digit]),) # change the LSB of the alpha channel
+                digit += 1
+            else:
+                newpix = item
+            newData.append(newpix)
 
-    # Only modify the alpha channel
-    for i in range(0, min(message_len, len(flat_data) // 4)):
-        flat_data[i4 + 3] = flat_data[i4 + 3] & ~1 | int(binary_message[i])
-
-    datas = flat_data.reshape(datas.shape)
-    img = Image.fromarray(datas, 'RGBA')
-    img.save(output_path, "PNG")
-
-    return "Message encoded successfully"
-
-def decode_image(img_path: str) -> str:
-    """
-    Decode a message from the alpha channel of an image.
-    """
-    try:
-        img = Image.open(img_path)
-    except IOError:
-        return "Error opening image file."
-
-    if img.mode != 'RGBA':
-        img = img.convert('RGBA')
-
-    datas = np.array(img)
-    binary_message = ''.join(str(datas[i // 4][i % 4][3] & 1) for i in range(0, datas.size // 4))
-    end_delimiter = binary_message.find('1111111111111110')
-
-    if end_delimiter != -1:
-        binary_message = binary_message[:end_delimiter]
-        return bin_to_message(binary_message)
+        img.putdata(newData)
+        img.save(output_path, "PNG")
+        return "Message encoded successfully"
     else:
-        return "Message not found or incorrect image mode."
+        return "Incorrect image mode, couldn't encode"
+
+# Function to decode the message from an image
+def decode_image(img_path):
+    img = Image.open(img_path)
+    binary_message = ""
+    if img.mode in ('RGBA'):
+        img = img.convert('RGBA')
+        datas = img.getdata()
+
+        for item in datas:
+            digit = item[3] & 1 # extracting the LSB of the alpha channel
+            binary_message += str(digit)
+            if binary_message[-16:] == '1111111111111110': # check if we've reached the delimiter
+                break
+
+        binary_message = binary_message[:-16] # remove the delimiter
+        message = bin_to_message(binary_message)
+        return message
+    else:
+        return "Incorrect image mode, couldn't decode"
+
+
+#print(encode_image('./panda.png', 'This is Dev & Atharva', './enimage.png'))
+print(decode_image('./enimage.png'))
